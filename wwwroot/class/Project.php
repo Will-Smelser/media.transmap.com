0a002ad1d86e51cb44e79760573d1aa221d14c01
Will Smelser <willsmelser@gmail.com>
2012-09-15 18:34:07 -0400

    fixed the conflict with Project

--- wwwroot/class/Project.php
+++ wwwroot/class/Project.php
@@ -2,145 +2,196 @@
 
 class Project{
 	private $HOST_IMG = '';
-	private $projectMap =  array(
-			'Alexandria'=>"Media10/Alexandria",
-			'Greenburgh'=>'Media10/Greenburgh',
-			'ElPaso'=>'Media3/ElPaso/Images',
-			'Allegheny'=>'Media10/Allegheny/Images',
-			'SeaTac'=>'Media2/SeaTac/Images',
-			'Fresno'=>'Media10/Fresno/Images',
-			'VCDD'=>'Media10/VCDD/Images',
-			'Sarasota'=>'Media10/Sarasota',
-			'I595'=>'',
-			'Schertz'=>'Media10/Schertz',
-			'ICA'=>'Media3/ICA',
-			'Manatee','Media3/Manatee',
-			'redwoodcity'=>'Media10/Redwood',
-			'Irvine'=>'Media10/Irvine',
-			'HuberHeights'=>'Media10/Huber_Heights',
-			'PutnamCounty'=>'Media10/PutnamCounty',
-			'Watertown'=>'Media10/Watertown',
-			'JIMI'=>'Media10/JIMI',
-			'ErieCounty'=>'Media10/Erie',
-			'Buffalo'=>'Media10/Buffalo',
-			'Casper'=>'Media11/Casper',
-			'Wilmington'=>'Media11/Wilmington',
-			'Lakeland'=>'Media11/Lakeland',
-			'SIPOA'=>'Media11/SIPOA',
-			'Cary'=>'Media11/Cary',
-			'Milton'=>'/images/Milton',
-			'Escambia'=>'Media11/Escambia',
-			'Kettering'=>'Media11/Kettering',
-			'Hanford'=>'Media11/Hanford',
-			'Conroe'=>'/images/conroe'
-	);
-	
-	
+	private $projectMap =  array();
+
 	private $projectName;
 	private $projectPath;
 	private $survey;
-	
+
 	//each image is just a number
 	private $imagePos;
 	private $imageStr;
-	
-	private $noImage = "/images/default/no-survey-image.jpg";
-	
+
+	private $properties = '/Surveys/projects.properties';
+	private $noImage = '/images/default/no-survey-image.jpg';
+	private $surveyImages = '/images';
+
 	private $imageLinkUrlFr;
 	private $imageLinkUrlFl;
 	private $imageLinkUrlBr;
-	
+
 	private $imageUrlFr;
 	private $imageUrlFl;
 	private $imageUrlBr;
 	
-	function Project($projectName, $survey, $image){
+	private $session;
+
+	function Project($projectName, $survey, $image, Session &$session){
+		$this->session = &$session;
+		
+		if(!$session->isRegistered($this)){
+			$session->register($this);
+			//build project list
+			$handle = @fopen($_SERVER['DOCUMENT_ROOT'].$this->properties, "r");
+			while(($buffer = fgets($handle)) !== false){
+				$parts = explode(":",trim($buffer));
+				$this->projectMap[$parts[0]] = '/images'.$parts[1];
+				
+			}
+			fclose($handle);
+			$session->setNameValue($this, 'projectMap', $this->projectMap);
+		} else {
+			$this->projectMap = $session->getValue($this, 'projectMap');
+		}
 		
 		//make sure the key exists
 		if(!array_key_exists($projectName,$this->projectMap)){
 			throw new Exception("Project name failed to map to project directory.");
 		}
-		
+
 		//$this->HOST_IMG = 'http://'.$_SERVER['HTTP_HOST'];
 		
+		if(empty($survey) || empty($image)){
+			if(empty($survey))
+				$survey = $this->findFirstSurvey($projectName);
+			
+			if(!empty($survey) && empty($image))
+				$image = $this->findFirstImage($projectName, $survey);
+			
+			header('Location: '.$_SERVER['PHP_SELF'].'?Image='.$image.'&Project='.$projectName.'&Survey='.$survey);
+		}		
+
 		$this->projectName = $projectName;
 		$this->projectPath = $this->projectMap[$projectName];
 		$this->survey = $survey;
-		
+
 		$this->imagePos = intval($image);
 		$this->imageStr = str_pad(strval($this->imagePos),5,'0',STR_PAD_LEFT).'.jpg';
-		
+
 		$this->imageLinkUrlFl = $this->getImageLinkUrl('FL');
 		$this->imageLinkUrlFr = $this->getImageLinkUrl('RF');
 		$this->imageLinkUrlBr = $this->getImageLinkUrl('BR');
-		
+
 		$this->imageUrlFr = $this->getImageResizedUrl('RF',25);
 		$this->imageUrlFl = $this->getImageResizedUrl('FL',25);
 		$this->imageUrlBr = $this->getImageResizedUrl('BR',18);
+
+	}
+
+	private function findFirstSurvey($project){
+		$base = $_SERVER['DOCUMENT_ROOT'].$this->surveyImages.'/'.$project;
+		
+		foreach(scandir($base) as $file){
+			if($file[0] != '.' && is_dir($base.'/'.$file)){
+				return $file;
+			}
+		}
+		return 'failed';
+	}
+	
+	private function findFirstImage($project, $survey){
+		$base = $_SERVER['DOCUMENT_ROOT'].$this->surveyImages.'/'.$project.'/'.$survey;
+		foreach(scandir($base) as $file){
+			if($file[0] != '.' && is_dir($base.'/'.$file)){
+				foreach(scandir($base.'/'.$file) as $img){
+					if(preg_match('/(\.jpe?g)$/i',$img)){
+						$basefile = basename($img);
+						$parts = explode('_',$basefile);
+						if(isset($parts[1]))
+							return preg_replace('/\.jpe?g/i','',$parts[1]);
+						
+						return 'failed';
+					}
+				}
+			}
+		}
+		
+		return 'failed';
+	}
+	
+	private function getImageLinkUrl($prefix='FL'){
+		$temp = null;
+		
+		if(!empty($this->HOST_IMG)){
+			return "{$this->HOST_IMG}{$this->projectPath}/{$this->survey}/$prefix{$this->survey}/{$prefix}_{$this->imageStr}";
+		} else {
+			$temp = rtrim($_SERVER['DOCUMENT_ROOT'],'/\\')."/{$this->projectPath}/{$this->survey}/$prefix{$this->survey}/{$prefix}_{$this->imageStr}";
+			if(file_exists($temp)){
+				return str_replace($_SERVER['DOCUMENT_ROOT'].'/','',$temp);
+			} else {
+				return $this->noImage;
+			}
+		}
 		
 	}
 
-	private function getImageLinkUrl($prefix='FL'){
-		$temp = "{$this->HOST_IMG}{$this->projectPath}/{$this->survey}/$prefix{$this->survey}/{$prefix}_{$this->imageStr}";
-		
-		//if(file_exists($temp)){
-			return $temp;
-		//}
-		//return $this->noImage;
-	}
-	
 	private function getImageResizedUrl($prefix='FL',$percent=25){
-		$temp = "/images/percent/{$percent}".$this->getImageLinkUrl($prefix);
+		$temp = $this->HOST_IMG."/imgsize.php?percent={$percent}&img=".$this->getImageLinkUrl($prefix);
 		return $temp;
 	}
-	
+
 	public function getProjectName(){
 		return $this->projectName;
 	}
-	
+
 	public function getSurvey(){
 		return $this->survey;
 	}
-	
+
 	public function getProjectPath(){
 		return $this->projectPath;
 	}
-	
+
 	public function getImagePadded(){
 		return str_pad(strval($this->imagePos),5,'0',STR_PAD_LEFT);
 	}
-	
+
 	public function getImageLinkFr(){
 		return $this->imageLinkUrlFr;
 	}
-	
+
 	public function getImageLinkFl(){
 		return $this->imageLinkUrlFl;
 	}
-	
+
 	public function getImageLinkBr(){
 		return $this->imageLinkUrlBr;
 	}
-	
+
 	public function getImageFr(){
 		return $this->imageUrlFr;
 	}
-	
+
 	public function getImageFl(){
 		return $this->imageUrlFl;
 	}
-	
+
 	public function getImageBr(){
 		return $this->imageUrlBr;
 	}
-	
+
 	public function getNextImageUrl($offset){
-		$pos = $this->imagePos + $offset;
+		$pos = $this->imagePos + intval($offset);
 		$str = str_pad(strval($pos),5,'0',STR_PAD_LEFT);
-		
+
 		return "/Surveys/index.php?Image={$str}&Project={$this->projectName}&Survey={$this->survey}";
 	}
 	
+	public function hasProjectImages($projectLink){
+		$parts = parse_url($projectLink);
+		
+		parse_str($parts['query']);
+		
+		$path = "{$this->projectPath}/{$this->survey}/FL{$this->survey}/FL_{$Image}.jpg";
+		$fullpath = null;
+		
+		if(empty($this->HOST_IMG)){
+			return file_exists($_SERVER['DOCUMENT_ROOT'].$path);
+		} else {
+			return file_exists($this->HOST_IMG.$path);
+		}
+		
+	}
 }
 
 ?>
