c74b83159b393a43b1cf662f6696bac7d8830ef4
Will-Smelser <Will1480>
2014-02-01 15:29:30 -0500

    Got the Oauth example finally working.

---
+++ wwwroot/oauth2/client.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include '../includes/html/css.html'; ?>
+</head>
+<body style="overflow: hidden">
+    <!-- begin header -->
+    <div id="header" class="span-24" style="padding-top:24px;">
+        <!-- site logo -->
+        <a href="http://transmap.com/" title="Home"><img class="logo" src="../blueprint/themes/amadou/logo.png" alt="Home" border="0" /></a>
+
+        <p style="align:right;"><a href="http://www.transmap.com/?page_id=2"><span style="color:white">Help</span></a> | <a href="http://www.transmap.com/?page_id=2"><span style="color:white">Contact</span></a></p>
+    </div>
+    <div style="padding:20px">
+    <!-- end header -->
+    <h2>Please Wait...</h2>
+    <p>Application is processing token.</p>
+    </div>
+</body>
