ba2cb83243daa9bd8535daa42781033543bdf10c
Will-Smelser <Will1480>
2014-06-17 21:46:50 -0400

    Added a bunch of images

--- wwwroot/newsite/services.php
+++ wwwroot/newsite/services.php
@@ -5,6 +5,16 @@
 
     <?php include 'includes/head.php'; ?>
 
+    <!-- Magnific Popup core CSS file -->
+    <link rel="stylesheet" href="libs/popup/magnific-popup.css">
+
+    <style>
+        .img-thumbnail.mleft{
+            margin-left:10px;
+        }
+    </style>
+
+
 </head>
 
 <?php include 'includes/navigation.php'; ?>
@@ -12,33 +22,149 @@
 <div class="container">
     <h1>Our Services</h1>
 
-    <h2>ON-SIGHT™ Mobile Mapping</h2>
-    <p>Transmap uses its ON-SIGHT™ HD Mobile Mapping System to capture ground-based LiDAR, 360 degree street-level, photogrammetric images and a dedicated HD camera scanning for pavement distresses.  The ON-SIGHT™ HD Mobile Mapping System collects data at normal driving speeds so special traffic control devices are not required during data collection.</p>
+    <div class="row">
+        <div class="col-lg-6">
+            <h2>ON-SIGHT™ Mobile Mapping</h2>
+            <p>Transmap uses its ON-SIGHT™ HD Mobile Mapping System to capture ground-based LiDAR, 360 degree street-level, photogrammetric images and a dedicated HD camera scanning for pavement distresses.  The ON-SIGHT™ HD Mobile Mapping System collects data at normal driving speeds so special traffic control devices are not required during data collection.</p>
+        </div>
+        <div class="col-lg-6">
+            <a  class="image-link" href="images/services/large/on-sight-van-1.jpg" target="_blank">
+                <img src="images/services/small/on-sight-van-1.jpg" class="img-thumbnail" />
+            </a>
 
-    <h2>Pavement Management</h2>
-    <p>
-        After the data collection process is completed, Transmap will assess the condition of all roadways in a network using the digital image database captured during raw data collection in the field.  Trained pavement technicians rate both the type of distress (alligator cracking, edge cracking, potholes, etc.), as well as the severity of each distress, following the guidelines established by the ASTM D6433 standards.
-    </p>
-    <p>
-        Transmap uses a profilometer to collect rutting and ride data.  The profiler meets all ASTM E950 standards.  The International Roughness Index (IRI) is collected for the left wheel track, the right wheel track, and the average of the two wheel tracks.
-    </p>
+            <a  class="image-link" href="images/services/large/on-sight-van-2.jpg" target="_blank">
+                <img src="images/services/small/on-sight-van-2.jpg" class="img-thumbnail mleft" />
+            </a>
+
+            <a  class="image-link" href="images/services/large/on-sight-lidar.jpg" target="_blank">
+                <img src="images/services/small/on-sight-lidar.jpg" class="img-thumbnail mleft" />
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <h2>Pavement Management</h2>
+        <div class="col-lg-4">
+            <a class="image-link" href="images/services/large/pavement-mgt-mr-table.jpg" target="_blank">
+                <img src="images/services/small/pavement-mgt-mr-table.jpg" class="img-thumbnail" />
+            </a>
+        </div>
+        <div class="col-lg-8">
+
+            <p>
+                After the data collection process is completed, Transmap will assess the condition of all roadways in a network using the digital image database captured during raw data collection in the field.  Trained pavement technicians rate both the type of distress (alligator cracking, edge cracking, potholes, etc.), as well as the severity of each distress, following the guidelines established by the ASTM D6433 standards.
+            </p>
+            <p>
+                Transmap uses a profilometer to collect rutting and ride data.  The profiler meets all ASTM E950 standards.  The International Roughness Index (IRI) is collected for the left wheel track, the right wheel track, and the average of the two wheel tracks.
+            </p>
+
+            <div>
+                <a class="image-link" href="images/services/large/pavement-mgt-map-1.jpg" target="_blank">
+                    <img src="images/services/small/pavement-mgt-map-1.jpg" class="img-thumbnail" />
+                </a>
+                <a class="image-link" href="images/services/large/pavement-mgt-map-2.jpg" target="_blank">
+                    <img src="images/services/small/pavement-mgt-map-2.jpg" class="img-thumbnail mleft" />
+                </a>
+                <a class="image-link" href="images/services/large/pavement-mgt-map-3.jpg" target="_blank">
+                    <img src="images/services/small/pavement-mgt-map-3.jpg" class="img-thumbnail mleft" />
+                </a>
+                <a class="image-link" href="images/services/large/pavement-mgt-map-4.jpg" target="_blank">
+                    <img src="images/services/small/pavement-mgt-map-4.jpg" class="img-thumbnail mleft" />
+                </a>
+            </div>
+
+        </div>
+    </div>
+
     <h2>Asset Management</h2>
     <p>
         We have been collecting roadway assets for over 20 years with the ability to inventory the assets that provide cost savings for our customers.  Some of these assets include traffic signs, traffic signals and poles, sidewalks, curbs, manholes, water valves, parking meters, hydrants, power vaults, gas valves, survey monuments, catch basins, light poles, drop inlets, pavement markings, guide rails, culverts, ditches, speed zones, school zones, cross walks, and street trees.
     </p>
-    <h3>Sign Inventory</h3>
-    <p>
-        Through our technology and our expert employees, Transmap can meet and exceed the data requirements for the condition assessment of all signs.  These services include, but are not limited to: Sign location (within 1 meter), sign orientation, sign position, MUTCD code, standard width and height, sign structure, post structure condition, sign condition, etc.
-    </p>
+    <div class="row text-center">
+        <div class="col-lg-4">
+            <div class="panel panel-default">
+            <div class="panel-heading"><b>Existing Sidewalks</b></div>
+            <div class="panel-body">
+                <a class="image-link" target="_blank" href="images/services/large/asset-mgt-map-1.jpg">
+                    <img src="images/services/small/asset-mgt-map-1.jpg" />
+                </a>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Missing Sidewalks</b></div>
+                <div class="panel-body">
+                    <a class="image-link" target="_blank" href="images/services/large/asset-mgt-map-2.jpg">
+                        <img src="images/services/small/asset-mgt-map-2.jpg" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-lg-5">
+            <h3>Sign Inventory</h3>
+            <p>
+                Through our technology and our expert employees, Transmap can meet and exceed the data requirements for the condition assessment of all signs.  These services include, but are not limited to: Sign location (within 1 meter), sign orientation, sign position, MUTCD code, standard width and height, sign structure, post structure condition, sign condition, etc.
+            </p>
+        </div>
+
+        <div class="col-lg-7">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading"><b>Images and LiDAR Representation</b></div>
+                <div style="display: inline-block;padding:10px;">
+                    <a class="image-link" target="_blank" href="images/services/large/sign-inventory-1.jpg">
+                        <img src="images/services/small/sign-inventory-1.jpg" />
+                    </a>
+                </div>
+                <div style="display: inline-block;padding:10px;">
+                    <a class="image-link" target="_blank" href="images/services/large/sign-inventory-2.jpg">
+                        <img src="images/services/small/sign-inventory-2.jpg" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
     <h3>Nighttime Sign Inspections & Retroreflectivity</h3>
     <p>
         Transmap can use the MUTCD nighttime visual inspection requirements to assess the retroreflectivity of each sign along with a daytime condition assessment.
     </p>
+    <div class="row">
+        <div class="col-lg-12">
+        <div class="img-thumbnail">
+            <a class="image-link mleft" target="_blank" href="images/services/large/nighttime-1.jpg">
+                <img src="images/services/small/nighttime-1.jpg" />
+            </a>
+        </div>
+        <div class="img-thumbnail mleft">
+            <a class="image-link mleft" target="_blank" href="images/services/large/nighttime-2.jpg">
+                <img src="images/services/small/nighttime-2.jpg" />
+            </a>
+        </div>
+        </div>
+    </div>
 
-    <h2>Web GIS</h2>
+    <h2>Web GIS <img src="images/services/web-gis-collector-app.png" title="Web GIS Collector App" /></h2>
     <p>
         Transmap has been a Public Works Solutions Partner with Esri for over 20 years.  We are extremely well-versed in Geographic Information Systems (GIS), and how to best utilize GIS tools in the infrastructure management needs of our customers. With an increasingly changing software environment, Transmap’s open source policy allows for data to be seamlessly implemented into dozens of software systems.
     </p>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="img-thumbnail">
+                <a class="image-link mleft" target="_blank" href="images/services/large/web-gis-1.jpg">
+                    <img src="images/services/small/web-gis-1.jpg" />
+                </a>
+            </div>
+            <div class="img-thumbnail mleft">
+                <a class="image-link mleft" target="_blank" href="images/services/large/web-gis-2.jpg">
+                    <img src="images/services/small/web-gis-2.jpg" />
+                </a>
+            </div>
+        </div>
+    </div>
 
 </div>
 
@@ -51,4 +177,17 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
+
+<!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<!-- Magnific Popup core JS file -->
+<script src="libs/popup/jquery.magnific-popup.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('.image-link').magnificPopup({type:'image'});
+    });
+</script>
+
 </html>
