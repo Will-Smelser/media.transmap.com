aafad4536049b77188ca40608a398020b82426ab
Will-Smelser <willsmelser@gmail.com>
2017-01-06 22:47:45 -0500

    Address update per Colleen

--- wwwroot/newsite/includes/foot.php
+++ wwwroot/newsite/includes/foot.php
@@ -10,8 +10,8 @@
 <div class="footer">
     <h4>Transmap Corporation</h4>
     <p>
-        3366 Riverside Dr., Suite 103<br/>
-        Upper Arlington, OH 43221<br/>
+        5030 Transamerica Drive<br/>
+        Columbus, OH 43228<br/>
         (P) 614.481.6799<br/>
         (F) 614.481.4017
 
